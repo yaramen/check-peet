5bc30a361d658f6ca1d236fb4520df5e452eaad4
Mihail Yarantsev <mihaalich2@mail.ru>
2020-10-21 14:46:32 +0300

    add Product migration

---
+++ database/migrations/2020_10_21_114337_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->float('protein')->default(0);
+            $table->float('fat')->default(0);
+            $table->float('carbohydrate')->default(0);
+            $table->float('calories')->default(0);
+            $table->string('image')->default('');
+            $table->text('description')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
